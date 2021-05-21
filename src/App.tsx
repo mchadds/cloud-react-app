465a138d82128b872743e4bd4e6b797978a50849
Chad Dundas Smith <cdundassmith@gmail.com>
2021-05-21 14:34:14 -0600

    attempt redeployment

--- src/App.tsx
+++ src/App.tsx
@@ -144,7 +144,6 @@ const App = (() => {
           <button onClick={createNFT}>Create NFT</button>
           <div style={{marginBottom: 30}}>
             {
-              // change
               NFTs.map((nft: NFT) => (
                 <div key={nft.id || nft.name}>
                   <h2>{nft.name}</h2>
